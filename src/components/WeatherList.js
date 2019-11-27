1865add203417c7c5dc987c2e4670580a4e093b3
DaneWoods <megadster@comcast.net>
2019-11-26 16:52:21 -0800

    bug with not displaying days

---
+++ src/components/WeatherList.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import WeatherListItem from './WeatherListItem.js';
+import './App.css';
+
+class WeatherList extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render () {
+    const { forecastDays } = this.props;
+    return (
+        <div className="weather-list flex-parent">
+            { forecastDays.map((forecastDay, index) =>
+                <WeatherListItem
+                    key={forecastDay.dt}
+                    forecastDay={forecastDay}
+                    index={index}
+                />
+            ) }
+        </div>
+    );
+  }
+}
+
+export default WeatherList;
